9b01fd469687a0256d07af8efa3a554491a6112e
aq53 <abdulqadar52@gmail.com>
2021-01-16 01:32:29 +0500

    refactor insertRows code

--- bqHandlers/insertRow.js
+++ bqHandlers/insertRow.js
@@ -5,40 +5,42 @@ const { listTables } = require("./listTables");
 const updateSchema = require("./updateSchema");
 
 async function insertRow(tableId, row) {
-  const tables = await listTables();
-  if (tables.includes(tableId)) {
-    try {
-      await updateSchema(tableId, row);
-    } catch (err) {
-      console.log("Update Schema ERR::", err);
-    }
-  } else {
-    try {
-      await createTable(tableId, row);
-    } catch (err) {
-      console.log("Create Table ERR::", err);
-    }
-  }
-
-  setTimeout(async () => {
-    try {
-      const tableRow = {};
-      for (let key in row) {
-        if (typeof row[key] === "object") {
-          console.log(typeof row[key], key);
-          tableRow[camelize(key)] = JSON.stringify(row[key]);
-        } else {
-          tableRow[camelize(key)] = row[key];
-        }
+  return new Promise((resolve,reject)=>{
+    const tables = await listTables();
+    if (tables.includes(tableId)) {
+      try {
+        await updateSchema(tableId, row);
+      } catch (err) {
+        console.log("Update Schema ERR::", err);
       }
-      await bigquery.dataset(datasetId).table(tableId).insert([tableRow]);
-    } catch (err) {
-      console.log("INSERT ROW::", err);
-      throw err;
+    } else {
+      try {
+        await createTable(tableId, row);
+      } catch (err) {
+        console.log("Create Table ERR::", err);
+      }
     }
-  }, 5000);
-
-  return { data: row, tableId };
+  
+    setTimeout(async () => {
+      try {
+        const tableRow = {};
+        for (let key in row) {
+          if (typeof row[key] === "object") {
+            console.log(typeof row[key], key);
+            tableRow[camelize(key)] = JSON.stringify(row[key]);
+          } else {
+            tableRow[camelize(key)] = row[key];
+          }
+        }
+        await bigquery.dataset(datasetId).table(tableId).insert([tableRow]);
+        resolve({ data: row, tableId });
+      } catch (err) {
+        console.log("INSERT ROW::", err);
+        reject(err);
+      }
+    }, 5000);
+  
+  })
 }
 
 module.exports = insertRow;
