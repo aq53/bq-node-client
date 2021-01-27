2b6e61366aeb9030460003dcaf7642c2a239d1be
aq53 <abdulqadar52@gmail.com>
2021-01-27 20:09:11 +0500

    Post data into hal_validation_sum table

--- bqHandlers/insertRow.js
+++ bqHandlers/insertRow.js
@@ -6,31 +6,35 @@ const updateSchema = require("./updateSchema");
 const uuid = require("uuid").v4;
 
 async function insertIntoRelationTable(tableRow) {
-  const coCreation = tableRow["coCreation"];
-  const secondaryEffects = tableRow["secondaryEffects"].toLowerCase();
-  const gtmStrategy = tableRow["gtmStrategy"].toLowerCase();
-  const geographicalMarkets = JSON.parse(
-    tableRow["geographicalMarkets"].toLowerCase()
-  );
-  const fundraisingRound = tableRow["fundraisingRound"].toLowerCase();
-  const salesRevenueLastMonth = tableRow["salesRevenueLastMonth"];
+  const { postId, coCreation, salesRevenueLastMonth } = tableRow;
+  let {
+    secondaryEffects,
+    gtmStrategy,
+    geographicalMarkets,
+    fundraisingRound,
+  } = tableRow;
+  secondaryEffects = secondaryEffects.toLowerCase();
+  gtmStrategy = gtmStrategy.toLowerCase();
+  geographicalMarkets = JSON.parse(geographicalMarkets.toLowerCase());
+  fundraisingRound = fundraisingRound.toLowerCase();
+  
   const row1 = {
-    postId: tableRow.postId,
+    postId,
     statusText: "Co Creation",
     statusValue: coCreation ? 1 : 0,
   };
   const row2 = {
-    postId: tableRow.postId,
+    postId,
     statusText: "Secondary Effects",
     statusValue: secondaryEffects === "yes" ? 1 : 0,
   };
   const row3 = {
-    postId: tableRow.postId,
+    postId,
     statusText: "GTM strategy",
     statusValue: gtmStrategy.includes("network") ? 1 : 0,
   };
   const row4 = {
-    postId: tableRow.postId,
+    postId,
     statusText: "Geo",
     statusValue:
       geographicalMarkets.includes("south east asia") ||
@@ -39,7 +43,7 @@ async function insertIntoRelationTable(tableRow) {
         : 0,
   };
   const row5 = {
-    postId: tableRow.postId,
+    postId,
     statusText: "Stage",
     statusValue:
       fundraisingRound === "pre-seed" ||
@@ -50,15 +54,21 @@ async function insertIntoRelationTable(tableRow) {
         : 0,
   };
   const row6 = {
-    postId: tableRow.postId,
+    postId,
     statusText: "Revenue",
     statusValue: salesRevenueLastMonth ? 1 : 0,
   };
-
+  const sumNetwork = row1.statusValue + row2.statusValue + row3.statusValue;
+  const sumInvest = row4.statusValue + row5.statusValue + row6.statusValue;
   await bigquery
     .dataset(datasetId)
     .table("hal_data_validation")
     .insert([row1, row2, row3, row4, row5, row6]);
+
+  await bigquery
+    .dataset(datasetId)
+    .table("hal_validation_sum")
+    .insert([{ sumNetwork, sumInvest, postId }]);
 }
 
 function insertRow(tableId, row) {
